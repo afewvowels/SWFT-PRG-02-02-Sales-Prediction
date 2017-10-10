48138905cb243578721680679bfbeffdffeab1bb
Keith Smith <32456218+afewvowels@users.noreply.github.com>
2017-10-10 18:20:45 -0400

    Initial  version tested and working

--- SWFT-PRG-02-02-Sales-Prediction/main.swift
+++ SWFT-PRG-02-02-Sales-Prediction/main.swift
@@ -5,8 +5,17 @@
 //  Created by Keith Smith on 10/10/17.
 //  Copyright © 2017 Keith Smith. All rights reserved.
 //
+//  The East Coast sales division of a company generates 58 percent
+//  of total sales. Based on that percentage, write a program that will
+//  predict how much the East Coast division will generate if the company
+//  has $8.6 million in sales this year.
 
-import Foundation
+let revenue = 8600000
+let percentRevenue = 0.58
 
-print("Hello, World!")
+var totalProfit: Double
 
+// Need to cast revenue as double to make multiplication work
+totalProfit = Double(revenue) * percentRevenue
+
+print(totalProfit)
